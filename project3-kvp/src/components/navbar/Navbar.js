e12cf396782648678e4db2d229e02979fe495e8e
boronflex <kyle_palmer@icloud.com>
2018-02-20 14:44:44 -0600

    rudimentary input working in react app

--- project3-kvp/src/components/navbar/Navbar.js
+++ project3-kvp/src/components/navbar/Navbar.js
@@ -1,14 +1,14 @@
 import React from "react";
-//import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom"; -need to set this up for persistence i think
 import "./Navbar.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props => 
 
   <nav>
-    <div class="nav-wrapper">
-      <a href="/" class="brand-logo">Total Station</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
+    <div className="nav-wrapper">
+      <a href="/" className="brand-logo">Total Station</a>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><a href="/inputs">Inputs</a></li>
         <li><a href="/search">Search</a></li>
         <li><a href="/interview">Interview</a></li>
